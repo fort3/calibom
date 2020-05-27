4fe0005f0d4cc91f5452ea829b6cd1d4d042659d
Fortune Sam Okon <macintosh@Macintoshs-MacBook-Pro.local>
2020-05-27 14:14:12 +0200

    Updates

---
+++ precache-manifest.9857de93bb7eb3f8f3d666cc6cf8f076.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "258b855928836b1eb123d0c6b0ac32b9",
+    "url": "/calibom/index.html"
+  },
+  {
+    "revision": "d9205e49bb8b8b4c0a28",
+    "url": "/calibom/static/css/main.bfb61c6b.chunk.css"
+  },
+  {
+    "revision": "d04851a78fcf609f38c0",
+    "url": "/calibom/static/js/2.40252618.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calibom/static/js/2.40252618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9205e49bb8b8b4c0a28",
+    "url": "/calibom/static/js/main.b94de8bd.chunk.js"
+  },
+  {
+    "revision": "81c7c7a32b664cc3de60",
+    "url": "/calibom/static/js/runtime-main.c0f5686e.js"
+  }
+]);
