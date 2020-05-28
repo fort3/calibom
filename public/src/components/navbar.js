5c4403e5320b7cf4d9630a3066023467be60a421
Fortune Sam Okon <macintosh@Macintoshs-MacBook-Pro.local>
2020-05-28 21:34:06 +0200

    Create react app for calibom.org and publish it

---
+++ public/src/components/navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Nav extends React.Component{
+  
+  render(){
+    return(<div className="navbar">
+    <nav>
+      <a className="logo" href="index.html"><h1 className="head"><span id="header">Cal</span><i className="fa fa-tree"></i>Bom.org</h1></a>
+          <ul>
+            <li><a href={this.props.projects}>Projects</a></li>
+            <li><a href={this.props.system}>System</a></li>
+            <li><a href={this.props.products}>Products</a></li>
+            <li><a href={this.props.intervention}>Intervention Areas</a></li>
+            <li><a href={this.props.about}>About Us</a></li>
+            <li><a href={this.props.contact}>Contact Us</a></li>
+          </ul>
+        </nav>
+     </div> 
+    )
+  }
+};
+export default Nav;
