b2e2df1431cb7fd66140c71786c6e49de1580ac7
Fortune Sam Okon <macintosh@Macintoshs-MacBook-Pro.local>
2020-05-29 23:40:11 +0200

    Update calibom

--- src/index.js
+++ src/index.js
@@ -4,12 +4,7 @@ import './index.css';
 import CaliApp from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <CaliApp />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<CaliApp />,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
